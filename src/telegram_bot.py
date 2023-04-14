5eed156e82a2f45442048e5e76a59517f5e3d0aa
alifeee <alifeeerenn@hotmail.com>
2023-04-14 18:32:14 +0100

    bot returns message when /start sent

--- src/telegram_bot.py
+++ src/telegram_bot.py
@@ -1,5 +1,6 @@
-import asyncio
-import telegram
+import logging
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 from dotenv import load_dotenv
 import os
 
@@ -12,11 +13,19 @@ except KeyError:
         "Please set the TELEGRAM_BOT_ACCESS_TOKEN and TELEGRAM_PERSONAL_CHAT_ID environment variables."
     )
 
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
 
-async def main():
-    bot = telegram.Bot(api_key)
-    async with bot:
-        await bot.send_message(chat_id, "Hello World")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I am a bot, please talk to me!")
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    application = ApplicationBuilder().token(api_key).build()
+
+    start_handler = CommandHandler('start', start)
+    application.add_handler(start_handler)
+
+    application.run_polling()
